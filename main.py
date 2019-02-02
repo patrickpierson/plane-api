7c0bd5a644895bae651f6a091f3303016ae7917d
Patrick Pierson <patrick@patrickpierson.us>
2019-02-01 20:56:38 -0500

    bulk insert and geohash now working

--- main.py
+++ main.py
@@ -1,11 +1,28 @@
 from datetime import datetime
 from elasticsearch import Elasticsearch
 from elasticsearch import exceptions
+from elasticsearch import helpers
 import glob
 import json
 
 ELASTICSEARCH_URL = 'localhost:9200'
 es = Elasticsearch()
+
+
+def create_index():
+    request_body = {
+            "mappings": {
+                 "aircraft": {
+                    "properties": {
+                        "location": {
+                            "type": "geo_point"
+                        }
+                    }
+                }
+            }
+        }
+    print("creating 'test' index...")
+    es.indices.create(index='test', body=request_body)
 
 
 def get_data():
@@ -42,6 +59,8 @@ def parse_aircrafts(aircrafts):
         i['time_seen'] = datetime.utcfromtimestamp(aircrafts['now'])
         i['file_name'] = aircrafts['file_name']
         i['epoch'] = aircrafts['now']
+        if 'lon' in i and 'lat' in i:
+            i['location'] = '%s,%s' % (i.get('lat'), i.get('lon'))
         if 'alt_baro' in i:
             i['alt_baro'] = str(i['alt_baro'])
         aircrafts_list.append(i)
@@ -57,15 +76,35 @@ def send_elasticsearch(aircrafts_list):
         except exceptions.RequestError as e:
             print(e)
 
-#Todo: https://github.com/vinsci/geohash
+
+def upload_data(aircrafts_list):
+    # Uploading info to the new ES
+    docs = []
+    for i in aircrafts_list:
+        entry_uuid = i['hex'] + '-' + i['time_seen'].strftime('%s')
+        header = {
+            "_index": "test",
+            "_type": "aircraft",
+            "_id": entry_uuid,
+            "_source": i
+        }
+        docs.append(header)
+    helpers.bulk(es, docs)
+    print("Written: " + str(len(docs)))
+
 
 def main():
     pass
 
 
 if __name__ == "__main__":
+    if es.indices.exists(index="test"):
+        print('Test index exists')
+    else:
+        create_index()
     for i in get_data():
         aircrafts = get_aircraft(i)
         aircrafts_list = parse_aircrafts(aircrafts)
-        send_elasticsearch(aircrafts_list)
+        #send_elasticsearch(aircrafts_list)
+        upload_data(aircrafts_list)
     # main()
